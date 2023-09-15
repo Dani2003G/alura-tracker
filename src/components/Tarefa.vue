bdb471faf07f90a926da350d8b2825e55eb224f5
Dani2003G <daniel0202almeida@outlook.com>
2023-09-15 16:11:36 -0300

    Criacao do componente tarefa, onde vai ficar salvo a tarefa quando o usuario apertar no stop

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">
+        Descrição da tarefa
+      </div>
+      <div class="column">
+        <CronometroTarefa :tempoEmSegundo="15"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import CronometroTarefa from "@/components/Cronometro.vue";
+
+export default defineComponent({
+  name: 'TarefaVue',
+  components: {CronometroTarefa},
+  data: () => ({
+
+  })
+})
+</script>
+
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
