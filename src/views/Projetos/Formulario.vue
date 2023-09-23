e5eb9ee96bbcb71f9ee350dc9443f4d679164963
Dani2003G <daniel0202almeida@outlook.com>
2023-09-23 17:01:03 -0300

    edicao do projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -21,14 +21,32 @@ import {useStore} from "@/store";
 
 export default defineComponent({
   name: 'FormularioProjeto',
+  props: {
+    id: {
+      type: String
+    }
+  },
   data: () => ({
     nomeDoProjeto: ''
   }),
   methods: {
     salvar() {
-      this.store.commit('ADICIONAR_PROJETO', this.nomeDoProjeto);
+      if(this.id) {
+        this.store.commit('ALTERA_PROJETO', {
+          id: this.id,
+          nome: this.nomeDoProjeto
+        })
+      } else {
+        this.store.commit('ADICIONAR_PROJETO', this.nomeDoProjeto);
+      }
       this.nomeDoProjeto = '';
       this.$router.push('/projetos')
+    }
+  },
+  mounted() {
+    if(this.id) {
+      const projeto = this.store.state.projetos.find(proj => proj.id === this.id);
+      this.nomeDoProjeto = projeto?.nome || ''
     }
   },
   setup() {
